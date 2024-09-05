751787ab4f256d7834328c064fc251f30368cbd2
Kenneth Gao <108561447+duckyfuz@users.noreply.github.com>
2024-09-06 01:44:19 +0800

    add hash output

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -1,6 +1,7 @@
 locals {
   resume_source  = "../kennethgao_resume.pdf"
   formatted_time = formatdate("DDMMYY-hhmmZZZ", timestamp())
-  resume_key     = format("resume_%s.pdf", filesha256(local.resume_source))
+  resume_hash    = filesha256(local.resume_source)
+  resume_key     = format("resume_%s.pdf", local.resume_hash)
   s3_origin_id   = "myS3Origin"
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
 output "cloudfront_domain_name" {
   value = aws_cloudfront_distribution.s3_distribution.domain_name
 }
+
+output "resume_hash" {
+  value = local.resume_hash
+}
