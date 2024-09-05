4f5cd5a483df687f858510689214430ab5ac9ff4
Kenneth Gao <108561447+duckyfuz@users.noreply.github.com>
2024-09-06 01:33:14 +0800

    use random string as default api token

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,11 @@
+resource "random_string" "cloudflare_api_token" {
+  length  = 40
+  special = false
+  upper   = true
+  lower   = true
+}
+
 variable "cloudflare_api_token" {
-    type = string
-    default = ""
+  type    = string
+  default = random_string.cloudflare_api_token.result
 }
