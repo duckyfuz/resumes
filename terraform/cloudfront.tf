84eaf3b5e8cda344946e685e453921efc930bedd
Kenneth Gao <108561447+duckyfuz@users.noreply.github.com>
2024-09-05 11:23:17 +0800

    split aws.tf into two

---
+++ terraform/cloudfront.tf
@@ -0,0 +1,72 @@
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name              = aws_s3_bucket.resume_bucket.bucket_regional_domain_name
+    origin_id                = local.s3_origin_id
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = local.resume_key
+
+  aliases = ["resume.kenf.dev"]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  # Cache behavior for PDF files
+  ordered_cache_behavior {
+    path_pattern     = "/${local.resume_key}"
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  price_class = "PriceClass_200"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = aws_acm_certificate_validation.cert.certificate_arn
+    ssl_support_method = "sni-only"
+  }
+}
+
+resource "aws_acm_certificate_validation" "cert" {
+  provider = aws.us-east-1
+  certificate_arn         = "arn:aws:acm:us-east-1:533267177082:certificate/af022575-6c3d-4075-96f1-52911405fdd4"
+}
---
+++ terraform/s3.tf
@@ -0,0 +1,46 @@
+resource "aws_s3_bucket" "resume_bucket" {
+  bucket = "resume-storage-bucket"
+}
+
+resource "aws_s3_bucket_public_access_block" "allow_public_acl" {
+  bucket = aws_s3_bucket.resume_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_object" "pdf_upload" {
+  bucket       = aws_s3_bucket.resume_bucket.bucket
+  key          = local.resume_key
+  source       = local.resume_source
+  content_type = "application/pdf"
+}
+
+resource "aws_s3_bucket_website_configuration" "example" {
+  bucket = aws_s3_bucket.resume_bucket.id
+
+  index_document {
+    suffix = local.resume_key
+  }
+}
+
+resource "aws_s3_bucket_policy" "resume_bucket_policy" {
+  bucket = aws_s3_bucket.resume_bucket.bucket
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.resume_bucket.arn}/${local.resume_key}"
+      }
+    ]
+  })
+
+  depends_on = [ aws_s3_bucket_public_access_block.allow_public_acl ]
+}
