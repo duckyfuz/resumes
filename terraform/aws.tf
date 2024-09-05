e44002ada1fb8142c53606a32f4708b9d0351ecd
Kenneth Gao <108561447+duckyfuz@users.noreply.github.com>
2024-09-06 01:38:43 +0800

    update resume only if file changes

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -1,6 +1,6 @@
 locals {
   resume_source  = "../kennethgao_resume.pdf"
   formatted_time = formatdate("DDMMYY-hhmmZZZ", timestamp())
-  resume_key     = format("kennethgao_resume_CAA%s.pdf", local.formatted_time)
+  resume_key     = format("resume_%s.pdf", filesha256(local.resume_source))
   s3_origin_id   = "myS3Origin"
 }
