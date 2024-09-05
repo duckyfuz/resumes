ec4dec4ba01508399a900595ffb59cfdb04b1305
Kenneth Gao <108561447+duckyfuz@users.noreply.github.com>
2024-09-05 22:45:22 +0800

    Revert "remove cloudflare provider"
    This reverts commit 9b3c3e5a76371067a20f4adad0413272955a834e.

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -1,6 +1,6 @@
-# provider "cloudflare" {
-#   api_token = var.cloudflare_api_token
-# }
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
 
 locals {
   cloudflare_zone_id = "47e2c26d25d25ff12cb21a0b2a5e1a4d"
