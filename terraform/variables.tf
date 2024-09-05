39abfc2532c24fee505437578350ba9aa8af8bea
Kenneth Gao <108561447+duckyfuz@users.noreply.github.com>
2024-09-06 01:35:33 +0800

    remove variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,4 @@
-resource "random_string" "cloudflare_api_token" {
-  length  = 40
-  special = false
-  upper   = true
-  lower   = true
-}
-
 variable "cloudflare_api_token" {
   type    = string
-  default = random_string.cloudflare_api_token.result
+  default = "a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r8s9ag"
 }
