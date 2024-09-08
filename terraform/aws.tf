5c5cc961308d0e0fe7749e4594e50f22d771d5d4
Kenneth Gao <108561447+duckyfuz@users.noreply.github.com>
2024-09-08 12:12:59 +0800

    refactoring locals

--- terraform/aws.tf
+++
@@ -1,7 +0,0 @@
-locals {
-  resume_source  = "../kennethgao_resume.pdf"
-  formatted_time = formatdate("DDMMYY-hhmmZZZ", timestamp())
-  resume_hash    = filesha256(local.resume_source)
-  resume_key     = format("resume_%s.pdf", local.resume_hash)
-  s3_origin_id   = "myS3Origin"
-}
