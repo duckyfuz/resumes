99aa5f5b71029233b9e8a987eb9fbf36f42be375
Kenneth Gao <108561447+duckyfuz@users.noreply.github.com>
2024-10-02 20:33:01 +0800

    output version hash

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,6 @@ output "cloudfront_domain_name" {
   value = aws_cloudfront_distribution.s3_distribution.domain_name
 }
 
-output "resume_hash" {
-  value = local.resume_hash
+output "version_hash" {
+  value = local.version_hash
 }
