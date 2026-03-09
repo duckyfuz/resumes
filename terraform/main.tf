b4035e120857c4f3a898921baa814df199f8529f
duckyfuz <108561447+duckyfuz@users.noreply.github.com>
2026-03-09 23:35:34 +0800

    fix: reintroduce aws provider for state cleanup

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,11 +6,24 @@ terraform {
   }
 
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
     cloudflare = {
       source  = "cloudflare/cloudflare"
       version = "4.40.0"
     }
   }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+provider "aws" {
+  region = "us-east-1"
+  alias  = "us-east-1"
 }
 
 provider "cloudflare" {
